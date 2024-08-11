b721d94432cbb31b4a7fa71bc0587300a45c0e0e
GiaHuy <tovugiahuy@gmail.com>
2024-08-11 23:53:46 +0700

    first commit

---
+++ src/main/Welcome.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Hello Tô Vũ Gia Huy !");
+		System.out.println("Welcome to the DHKTPM19ATT !");
+	}
+}
